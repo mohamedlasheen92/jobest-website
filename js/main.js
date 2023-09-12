7b4512be3e2c339cdcecf9adddb5a130a0abaea7
mohamedlasheen92 <mohamedlasheen637@gmail.com>
2023-09-12 16:57:29 +0300

    ADD Testimonials section

--- js/main.js
+++ js/main.js
@@ -10,3 +10,26 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > 0) header.style.backgroundColor = 'rgba(249, 250, 253, 0.7)';
   else header.style.backgroundColor = "transparent";
 })
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  grabCursor: true,
+  // spaceBetween: 10,
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
